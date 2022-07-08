0756f10b9d509d8e997d33222ad48cdfdd835ee7
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2022-07-08 15:36:53 +0900

    Update Todo.js

--- Todo.js
+++ Todo.js
@@ -1,4 +1,4 @@
-/*js는 대부분 구글링이지만 찾기 힘들었따..*/
+/*js힘들어-*/
 
 init();
 
@@ -12,7 +12,7 @@ function init(){
 
 function deleteOrCheck(e){
     if(e.target.className == 'delete')  
-        deleteToDo(e); // X 버튼을 누르면 목록에서 항목 삭제
+        deleteToDo(e);
     else {
         checkToDo(e); // 체크박스를 클릭한 경우 글씨 색을 연하게 바꿔준다.
     }
@@ -22,14 +22,14 @@ function clearTodoList(e){ //목록 전체 삭제하는 경우
     let ul = document.querySelector('ul').innerHTML = '';
 }
 
-function deleteToDo(e){ // X 버튼을 누르면 목록에서 항목 삭제
+function deleteToDo(e){ // X 버튼을 누르면 투두리스트항목 삭제.
     let remove = e.target.parentNode;
     let parentNode = remove.parentNode;
     parentNode.removeChild(remove);
 }
 
 
-function checkToDo(e){  // 체크박스를 클릭한 경우 글씨 색을 연하게 바꿔준다.
+function checkToDo(e){  // 체크박스를 클릭하면 글씨 색깔바꾸기.
     const todo = e.target.nextSibling;
     if(e.target.checked){
         todo.style.color = "#dddddd";
@@ -41,12 +41,12 @@ function checkToDo(e){  // 체크박스를 클릭한 경우 글씨 색을 연하
 
 
 
-function addToDo(e){ //새로운 할 일 추가하는 경우
+function addToDo(e){ //새로운 할 일 추가하기
     e.preventDefault();
     let toDoValue = document.querySelector('input');
     if(toDoValue.value !== '')
         addTask(toDoValue.value);
-        toDoValue.value = ''; //입력창 비워주기
+        toDoValue.value = ''; //입력창 비우기
 }
 
 function addTask(value){
